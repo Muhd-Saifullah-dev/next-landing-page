a33d79d2f9f641e5e773d68f96a64ad4f4082a17
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-01-29 14:24:16 +0500

    Header section add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { ArrowRight,Menu } from 'lucide-react'
+import Logo from "@/assets/logosaas.png"
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='sticky top-0'>
+    <div className='flex justify-center items-center py-3 bg-black text-white text-sm gap-3'>
+        <p className='text-white/60 hidden md:block'>Streamline your workflow and boost your productivity</p>
+        <div className='inline-flex gap-1 items-center'>
+        <p>Get started for free</p>
+        <ArrowRight className='h-4 w-4' />
+        </div>
+    </div>
+    <div className='py-5'>
+        <div className='container'>
+            <div className='flex items-center justify-between'>
+      <Image src={Logo} alt="saas logo" width={40} height={40}/>
+      <Menu className='h-5 w-5 md:hidden' />
+      <nav className='hidden md:flex gap-6 text-black/60 items-center'>
+        <a href='#' >About</a>
+        <a href='#' >Feature</a>
+        <a href='#' >Customers</a>
+        <a href='#' >Updates</a>
+        <a href='#' >Help</a>
+        <button className='bg-black px-4 text-white py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight '>Get for free</button>
+      </nav>
+      </div>
+      </div>
+    </div>
+    </header>
   )
 }
 
