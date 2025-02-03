944b6b09fdbe71143e9da65c6cd74a3ff9d0271f
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2025-02-03 21:46:59 +0500

    add ProductShowcase component

--- src/components/ProductShowcase.jsx
+++ src/components/ProductShowcase.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import ProductImage from "@/assets/product-image.png"
+import pyramid from "@/assets/pyramid.png"
+import TubeImage from "@/assets/tube.png"
 import Image from 'next/image'
 const ProductShowcase = () => {
   return (
-   <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24'>
+   <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
     <div className='container'>
+        <div className='max-w-[540px] mx-auto'>
         <div className='flex justify-center'>
         <div className='tag'>Boost Your Productivity</div>
         </div>
-        <h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text'>A more effective way to track progress</h2>
-        <p className='text-center'>Effortlessly turn your idea into a fully functional, responsive, Saas website in just minute with this template.</p>
-        <Image  src={ProductImage} alt="Product-Image" className=''/>
+        <h2 className='text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>A more effective way to track progress</h2>
+        <p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5'>Effortlessly turn your idea into a fully functional, responsive, Saas website in just minute with this template.</p>
+        </div>
+        <div className='relative'>
+        <Image  src={ProductImage} alt="Product-Image" className='mt-10'/>
+        <Image  src={pyramid}  alt="Pyramid-Image" height={262} width={262} className='hidden md:block absolute -right-36 -top-32'/>
+        <Image src={TubeImage} alt="tube-imge" className='hidden md:block absolute bottom-24 -left-36' height={242} />
+        </div>
     </div>
    </section>
   )
